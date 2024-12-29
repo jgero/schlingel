cda363bdcc1ac4a2db94d898b47d2bf648f37fa0
Johannes Gerold <git+commit@jgero.me>
2024-12-29 15:48:08 +0100

    spike: move to rust

--- treefmt.nix
+++ treefmt.nix
@@ -1,13 +1,9 @@
 {
   projectRootFile = "flake.nix";
   programs.nixpkgs-fmt.enable = true;
-  programs = {
-    gofmt.enable = true;
-    templ.enable = true;
-    typos.enable = true;
-    prettier = {
-      enable = true;
-      includes = [ "*.md" "*.yaml" "*.yml" ];
-    };
-  };
+  programs.rustfmt.enable = true;
+  programs.toml-sort.enable = true;
+  programs.typos.enable = true;
+  programs.yamlfmt.enable = true;
+  programs.prettier.enable = true;
 }
